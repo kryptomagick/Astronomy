572da72f753e1af0ffedf7f4629e503c5ebaedc6
Karl Zander <uvajda@protonmail.com>
2022-01-07 00:52:40 -0800

    Update

---
+++ Code/kzmoontime.py
@@ -0,0 +1,24 @@
+# KZMoonTime
+
+''' Ka Set Osiris '''
+''' Qea Channel A Channel the Voice '''
+
+K = 30
+E1 = 2
+E2 = 4
+O = 14
+Q = 8
+C0 = 2
+A = -1
+C1 = 2
+L = 11
+
+T1 = ((30 * 2 * 14) * (8 * 2 * 1 * 2 * 11))
+print(T1)
+T2 = ((30 * 4 * 14) * (8 * 2 * 1 * 2 * 11))
+print(T2)
+
+ET0 = ((K * E1 * O) * (Q * C0 * A * C0 * L))
+ET1 = ((K * E2 * O) * (Q * C1 * A * C1 * L))
+T = ((K * E1 * O) * (Q * C0 * A * C1 * L))
+print(T)
